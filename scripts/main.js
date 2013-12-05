44f603c015b1cf1ce3ca3bad093a992ab53113d0
aresthedevil <aresthedevil@gmail.com>
2013-12-05 11:56:57 +0100

    fixed an error

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,7 @@
 ﻿$(document).ready(function () {
     $('#body').empty();
     var url = "http://symfony.p181457.webspaceconfig.de/app_dev.php";
+    alert(url);
     $('#body').load(url).fail(function(){
     	alert("error");
     	
