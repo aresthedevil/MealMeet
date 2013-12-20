30b35f59f9d5dbf83cfaa0a555681930768937c1
aresthedevil <aresthedevil@gmail.com>
2013-12-20 10:57:33 +0100

    test

--- scripts/main.js
+++ scripts/main.js
@@ -3,13 +3,12 @@
 	
 });
 
-$("#test").click(function () {
+$("#test").click(function() {
     var url = "http://symfony.p181457.webspaceconfig.de";
     console.log(url);
-    $('#body').load(url, function(){
-    	console.log(url + " loaded");
-    }).fail(function(){
-    	console.log(url + " not loaded");
+    $('#body').load(url, function() {
+        console.log(url + " loaded");
+    }).fail(function() {
+        console.log(url + " not loaded");
     });
-})
-
+});
