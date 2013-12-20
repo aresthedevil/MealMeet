3118568da1584b56ddfdeac75a3104266c0945a7
aresthedevil <aresthedevil@gmail.com>
2013-12-20 22:57:58 +0100

    timeout

--- scripts/main.js
+++ scripts/main.js
@@ -1,25 +1,17 @@
 ﻿$(document).ready(function() {
 
-    $.fn.htmlorginal = $.fn.html;
-
-    $.fn.html = function(html, callback) {
-        this.htmlorginal(html);
-        if (typeof callback == "function") {
-            callback();
-        }
-    };
 
     var url = "http://symfony.p181457.webspaceconfig.de";
     console.log(url);
     $('#body').load(url, function() {
         $('#base').attr('href', url);
         var content = $('#body').html();
-        $('#body').html(content, function() {
+        $('#body').html(content);
+        setTimeout(function() {
             $('#body').show();
             $('#spinningwheel').hide();
             console.log(url + " loaded");
-        });
-
+        }, 1000);
 
     }).fail(function() {
         console.log(url + " not loaded");
