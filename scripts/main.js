7f68b78486f1c33552ffc25c5065be793e246315
aresthedevil <aresthedevil@gmail.com>
2013-12-20 14:06:30 +0100

    added spinning wheel

--- scripts/main.js
+++ scripts/main.js
@@ -2,8 +2,12 @@
     
 	    var url = "http://symfony.p181457.webspaceconfig.de";
 	    console.log(url);
-	    $('#body').load(url, function () {
-	        console.log(url + " loaded");
+    $('#body').load(url, function () {
+	        var base = $("<base></base>");
+	        base.attr('href', url);
+	        $('#head').append(base);
+        $('#spinningwheel').hide();
+	    	console.log(url + " loaded");
 	    }).fail(function () {
 	        console.log(url + " not loaded");
 	    });
