09de8149ac96b1847cd691312f139da97daa3da8
aresthedevil <aresthedevil@gmail.com>
2013-12-20 22:26:55 +0100

    test

--- scripts/main.js
+++ scripts/main.js
@@ -3,11 +3,12 @@
 	    var url = "http://symfony.p181457.webspaceconfig.de";
 	    console.log(url);
     $('#body').load(url, function () {
-	        $('#base').attr('href', url);
-            $('#spinningwheel').hide();
-            console.log(url + " loaded");
-        var content = $('#body').html();
+	        $('#base').attr('href', url);            
+	        var content = $('#body').html();
 	    	$('#body').html(content);
+	    	$('#body').show();
+	    	$('#spinningwheel').hide();
+            console.log(url + " loaded");
 	    }).fail(function () {
 	        console.log(url + " not loaded");
 	    });
