d78627ff606766e243bfd61ca51a23af9f89ba31
aresthedevil <aresthedevil@gmail.com>
2013-12-20 22:44:53 +0100

    redefined html

--- scripts/main.js
+++ scripts/main.js
@@ -1,17 +1,28 @@
-﻿$(document).ready(function () {
-    
-	    var url = "http://symfony.p181457.webspaceconfig.de";
-	    console.log(url);
-    $('#body').load(url, function () {
-	        $('#base').attr('href', url);            
-	        var content = $('#body').html();
-	    	$('#body').html(content, function(){
-	    		$('#body').show();
-	    	$('#spinningwheel').hide();
-	    	console.log(url + " loaded");
-	    	});
-	    }).fail(function () {
-	        console.log(url + " not loaded");
-	    });
-	});
+﻿$(document).ready(function() {
+
+    $.fn.htmlorginal = $.fn.html;
+
+    $.fn.html = function(html, callback) {
+        this.htmlorginal(html);
+        if (typeof callback == "function") {
+            callback();
+        }
+    };
+
+    var url = "http://symfony.p181457.webspaceconfig.de";
+    console.log(url);
+    $('#body').load(url, function() {
+        $('#base').attr('href', url);
+        var content = $('#body').html();
+        $('#body').html(content, function() {
+            $('#body').show();
+            $('#spinningwheel').hide();
+            console.log(url + " loaded");
+        });
+
+
+    }).fail(function() {
+        console.log(url + " not loaded");
+    });
+});
 
